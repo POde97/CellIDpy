2088947605e4b973f193366fe2cb5ae10291afb7
POde97 <69850262+POde97@users.noreply.github.com>
2023-03-27 16:38:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='Cell-IDpy',
+    name='CellIDpy',
     version='1.0',    
     description='mca based method for cell type labeling',
-    url=https://github.com/POde97/Cell-IDpy,
+    url=https://github.com/POde97/CellIDpy,
     author='Paolo Odello',
     author_email='paoloodeo.o@gmail.com',
     license='MIT license',
-    packages=['Cell-IDpy'],
+    packages=['CellIDpy'],
     install_requires=['mapply==0.1.21',
                       'scanpy==1.9.3',                     
                       ],
