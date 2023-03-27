6935b3069b054f390a466e97d783372d6b49435e
POde97 <69850262+POde97@users.noreply.github.com>
2023-03-27 17:07:54 +0200

    Update ProteinCoding.py

--- CellIDpy/ProteinCoding.py
+++ CellIDpy/ProteinCoding.py
@@ -1,13 +1,13 @@
 def MgProteinCoding():
   import pickle
-  with open("MgProteinCoding", "rb") as fp:   # Unpickling
+  with open("CellIDpy/MgProteinCoding", "rb") as fp:   # Unpickling
     l = pickle.load(fp)
   
   return l 
 
 def HgProteinCoding():
   import pickle
-  with open("HgProteinCoding", "rb") as fp:   # Unpickling
+  with open("CellIDpy/HgProteinCoding", "rb") as fp:   # Unpickling
     l = pickle.load(fp)
   
   return l 
